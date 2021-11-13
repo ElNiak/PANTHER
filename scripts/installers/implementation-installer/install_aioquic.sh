6d5c6a1859fbded29ca04560ab36b2c674a35456
Christophe Crochet <christophe.crochet@uclouvain.be>
2021-11-13 17:40:28 +0100

    update installers

--- scripts/installers/implementation-installer/install_aioquic.sh
+++ scripts/installers/implementation-installer/install_aioquic.sh
@@ -1,5 +1,6 @@
 cd $PROOTPATH/quic-implementations/aioquic
 export PYTHONPATH=$PWD
+source $HOME/.cargo/env
 python3 -m pip install setuptools_rust
 python3 -m pip install -e .
 python3 -m pip install aiofiles asgiref dnslib httpbin starlette wsproto
