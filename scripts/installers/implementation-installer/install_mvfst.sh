fe7a94b3d6b55cc3542a41935120d8cfa368b9e2
Christophe Crochet <christophe.crochet@uclouvain.be>
2021-11-13 17:36:43 +0100

    update installers

--- scripts/installers/implementation-installer/install_mvfst.sh
+++ scripts/installers/implementation-installer/install_mvfst.sh
@@ -1,5 +1,7 @@
-cp $PROOTPATH/ressources/tls-keys-patch.diff $PROOTPATH/quic-implementations/mvfst/
-cp $PROOTPATH/ressources/samples-build-patch.diff $PROOTPATH/quic-implementations/mvfst/
+cp -r $PROOTPATH/ressources/mvfst/generic $PROOTPATH/quic-implementations/mvfst/quic/samples/generic
+cp $PROOTPATH/ressources/mvfst/tls-keys-patch.diff $PROOTPATH/quic-implementations/mvfst/
+cp $PROOTPATH/ressources/mvfst/samples-build-patch.diff $PROOTPATH/quic-implementations/mvfst/
+cp $PROOTPATH/ressources/mvfst/EchoClient.h $PROOTPATH/quic-implementations/mvfst/quic/samples/echo/EchoClient.h
 cd $PROOTPATH/quic-implementations/mvfst/
 git checkout 36111c1
 git apply tls-keys-patch.diff
