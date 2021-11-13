00049f0f948cbcdc398d4986a7289717cb788ced
Christophe Crochet <christophe.crochet@uclouvain.be>
2021-11-13 17:43:03 +0100

    update installers

--- scripts/installers/implementation-installer/install_mvfst.sh
+++ scripts/installers/implementation-installer/install_mvfst.sh
@@ -1,4 +1,4 @@
-cp -r $PROOTPATH/ressources/mvfst/generic $PROOTPATH/quic-implementations/mvfst/quic/samples/generic
+cp -R $PROOTPATH/ressources/mvfst/generic $PROOTPATH/quic-implementations/mvfst/quic/samples/generic
 cp $PROOTPATH/ressources/mvfst/tls-keys-patch.diff $PROOTPATH/quic-implementations/mvfst/
 cp $PROOTPATH/ressources/mvfst/samples-build-patch.diff $PROOTPATH/quic-implementations/mvfst/
 cp $PROOTPATH/ressources/mvfst/EchoClient.h $PROOTPATH/quic-implementations/mvfst/quic/samples/echo/EchoClient.h
