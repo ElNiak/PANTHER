3cff507807ddbe84eda74270d1458b97580f8c88
Christophe Crochet <christophe.crochet@uclouvain.be>
2021-11-13 18:14:40 +0100

    update installers

--- scripts/installers/implementation-installer/install_lsquic.sh
+++ scripts/installers/implementation-installer/install_lsquic.sh
@@ -1,4 +1,5 @@
 cd $PROOTPATH/quic-implementations/boringssl
+export PATH="/go/bin:${PATH}"
 git checkout a2278d4d2cabe73f6663e3299ea7808edfa306b9 
 sudo apt-get update && \
 sudo apt-get install -y build-essential software-properties-common \
@@ -6,7 +7,6 @@ sudo apt-get install -y build-essential software-properties-common \
 
 cmake . &&  make
 export BORINGSSL=$PWD
-export PATH="/go/bin:${PATH}"
 
 cd $PROOTPATH/quic-implementations/lsquic
 git checkout v2.29.4
