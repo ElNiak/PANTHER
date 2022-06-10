7fd5b8f3cf67f4859d6a1fc8e0405003f0638335
ElNiak <christophe.crochet@student.uclouvain.be>
2022-06-10 12:04:06 +0200

    Update run_experiments.py

--- run_experiments.py
+++ run_experiments.py
@@ -320,7 +320,7 @@ def main():
                                                 shell=True, executable="/bin/bash").wait()
                         subprocess.Popen("/usr/bin/tail -2 " + ivy_out, 
                                                 shell=True, executable="/bin/bash").wait()
-                        subprocess.Popen("/usr/bin/tail $(lsof -i udp) >/dev/null 2>&1", 
+                        subprocess.Popen("/usr/bin/tail $(/usr/bin/lsof -i udp) >/dev/null 2>&1", 
                                                 shell=True, executable="/bin/bash").wait()
                         log.info("\tKill thsark")
                         subprocess.Popen("sudo /usr/bin/pkill tshark", 
