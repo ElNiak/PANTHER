5c776f011b6d855b2c5ee993b11fbd916b13247d
ElNiak <christophe.crochet@student.uclouvain.be>
2022-06-25 15:07:04 +0200

    Update install.sh

--- scripts/installers/install.sh
+++ scripts/installers/install.sh
@@ -21,8 +21,8 @@ sudo apt-get install --fix-missing  -y libssl-dev
 
 sudo apt install --fix-missing  -y software-properties-common
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt-get install --fix-missing  -y python3.7 python3.7-dev #python3.9-pip
-sudo apt-get install --fix-missing  -y python3.7-distutils
+sudo apt-get install --fix-missing  -y python3.8 python3.8-dev #python3.9-pip
+sudo apt-get install --fix-missing  -y python3.8-distutils
 
 sudo apt remove cmake -y
 sudo snap remove cmake
